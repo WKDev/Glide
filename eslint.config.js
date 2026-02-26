d46d9b53bf8364b4932a4f6b373537056b3caf78
wkdev <chson@wklabs.io>
2026-02-26 10:36:21 +0900

    ci: add linting, formatting, and CI pipeline

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser,
+      },
+    },
+  },
+  {
+    ignores: [
+      'build/',
+      'node_modules/',
+      '.svelte-kit/',
+      'src-tauri/target/',
+      '.sisyphus/',
+    ],
+  },
+];
